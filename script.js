3ff99de76fdec51858401a59182a95bc50d3b943
rat00lis <ob.alarcon2001@gmail.com>
2024-02-21 17:26:22 -0300

    added clear and erase buttons. Clear is working
    clear button calls to clearDisplay() function which sets the display to ''
    and erase previous saved number so it can start over

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let CURRENT_OPERATION = null;
 let FIRST_NUMBER = null;
-let SECOND_NUMBER = null;
+let REPLACE_OPERATOR = false;
 let DISPLAY = document.querySelector(".display");
 let NEW_NUMBER = false;
 let ERROR_MESSAGE = 'error';
@@ -14,10 +14,13 @@ function cleanInput(input){
     return input.replace(/[^+\-\=\/x]/g, '');
 }
 
-function clearDisplay(){
+function clearDisplay(eraseNumbers = false){
+    if(eraseNumbers)FIRST_NUMBER = null;
     DISPLAY.textContent = '';
 }
-
+function eraseDisplay(){
+    
+}
 function operate(firstNumber, secondNumber, operator){
     //case switch needs a break line or
     //they might bug
@@ -50,7 +53,10 @@ function operatorPressed(operator){
         NEW_NUMBER = true;
         //
         CURRENT_OPERATION = operator.textContent;
-    }else{
+    
+    //only if you are not expecting a new number, you operate
+    // this allows you to change operator without operating
+    }else if (!NEW_NUMBER){
         //when there is two numbers, you operate
         let result = operate(parseInt(FIRST_NUMBER),secondNumber,CURRENT_OPERATION);
         clearDisplay(); //clear the display to output result
@@ -58,6 +64,8 @@ function operatorPressed(operator){
         NEW_NUMBER = true;
         FIRST_NUMBER = result; //and the result will acumulate next operations
         CURRENT_OPERATION = operator.textContent; 
+    }else{ //if you are only changing operator, then:
+        CURRENT_OPERATION = operator.textContent;
     }
 }
 
